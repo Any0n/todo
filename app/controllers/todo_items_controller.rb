9119d4cd0d147d4723ec5048f39339f31bc39110
Animesh Maharjan <animesh_maharjan@hotmail.com>
2016-04-21 20:21:01 +0545

    Finish styling

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -19,7 +19,8 @@ class TodoItemsController < ApplicationController
   end
 
   def complete
-
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item completed."
   end
 
   private
